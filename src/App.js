b3090626304b92d3813eeade9c9b981b09835c96
Youuu <31610705+73-yy@users.noreply.github.com>
2022-04-17 16:46:32 +0900

    make organisms

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
+import { PrimaryButton } from "./components/atoms/buttton/PrimaryButton";
+import { SecondaryButton } from "./components/atoms/buttton/SecondaryButton";
+import { SearchInput } from "./components/molecules/SearchInput";
+import { UserCard } from "./components/organisms/user/UserCard";
 import "./styles.css";
+
+const user = {
+  name: "jake",
+  image: "https://source.unsplash.com/NE0XGVKTmcA",
+  email: "12345@example.com",
+  phone: "000-9999-9999",
+  company: {
+    name: "テスト株式会社"
+  },
+  website: "httts://google.com"
+};
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <PrimaryButton>テスト</PrimaryButton>
+      <SecondaryButton>検索</SecondaryButton>
+      <SearchInput />
+      <UserCard user={user} />
     </div>
   );
 }
